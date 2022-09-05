d5a720dc70a95c3406fa61d9c22ed288e4c9285d
Vladimir <v0vanchikanna@gmail.com>
2022-09-05 17:40:40 +0300

    3Zadacha

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// Console.WriteLine("Введи 8 чисел");
+Console.Write("Введи количество эл-ов в массиве: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[count];
+
+for (int i = 0; i < array.Length ; i++)
+{
+    Console.WriteLine($"Введи элемент массива под индексом {i}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+Console.WriteLine("Вывод массива: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
